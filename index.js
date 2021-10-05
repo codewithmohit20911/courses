72a45154dba0268eff1571de59be33e160c69b2e
codewithmohit209 <90596153+codewithmohit20911@users.noreply.github.com>
2021-10-05 13:15:18 +0530

    update

---
+++ index.js
@@ -0,0 +1,18 @@
+    (function(d, m){
+        var kommunicateSettings = 
+            {"appId":"3e81e4f4d2807fa8a59f05694f4fdda39","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+/* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
+(function() {
+	var id = '797ad5d6-1f11-11ec-bccf-0242ac130002';
+	var ci_search = document.createElement('script');
+	ci_search.type = 'text/javascript';
+	ci_search.async = true;
+	ci_search.src = 'https://cse.expertrec.com/api/js/ci_common.js?id=' + id;
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(ci_search, s);
+  })();
